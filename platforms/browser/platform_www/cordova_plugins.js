02432a23674b3119eb10b76f673f3e67f80a14df
azarrabi <azarrabi@umich.edu>
2018-11-03 17:19:37 -0400

    merge

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -3,7 +3,8 @@ module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.3"
+    "cordova-plugin-whitelist": "1.3.3",
+    "phonegap-nfc": "1.0.3"
 }
 // BOTTOM OF METADATA
 });
